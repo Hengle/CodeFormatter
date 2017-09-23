3d26c7f3e7205ae0ce44a28040f56b59ceb52818
watanabe1115 <t.watanabe.1115@gmail.com>
2017-09-23 19:37:11 +0900

    FirstCommit

---
+++ CodeFormatter/src/FormattingOptions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using ICSharpCode.NRefactory.CSharp;
+
+namespace CodeFormatter
+{
+	public static class FormattingOptions
+	{
+		public static CSharpFormattingOptions DefaultOptions = FormattingOptionsFactory.CreateAllman();
+
+		public static void Load(string filePath = "settings.xml")
+		{
+			try
+			{
+				// 設定ファイルを開く（UTF-8 BOM無し）
+				using (var reader = new StreamReader(filePath, new System.Text.UTF8Encoding(false)))
+				{
+					//XmlSerializerオブジェクトを作成
+					System.Xml.Serialization.XmlSerializer serializer = new System.Xml.Serialization.XmlSerializer(typeof(CSharpFormattingOptions));
+					//XMLファイルから読み込み、逆シリアル化する
+					DefaultOptions = (CSharpFormattingOptions)serializer.Deserialize(reader);
+				}
+			}
+			catch
+			{
+				Save(filePath);
+			}
+		}
+
+		public static void Save(string filePath = "settings.xml")
+		{
+			// 書き込むファイルを開く（UTF-8 BOM無し）
+			using (var writer = new StreamWriter(filePath, false, new System.Text.UTF8Encoding(false)))
+			{
+				//オブジェクトの型を指定する
+				System.Xml.Serialization.XmlSerializer serializer = new System.Xml.Serialization.XmlSerializer(typeof(CSharpFormattingOptions));
+				//シリアル化し、XMLファイルに保存する
+				serializer.Serialize(writer, DefaultOptions);
+			}
+		}
+	}
+}
---
+++ CodeFormatter/src/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using ICSharpCode.NRefactory.CSharp;
+
+
+namespace CodeFormatter
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			if (args.Length <= 0)
+			{
+				return;
+			}
+			// コードフォーマットを行うファイル名を引数から取得
+			var targetFileName = args[0];
+			// ファイルが存在しないなら何もしない
+			if(!File.Exists(targetFileName))
+			{
+				return;
+			}
+
+			// オプションの設定を読み込む
+			FormattingOptions.Load();
+
+			// フォーマットを行うソースコードの中身が全て入る
+			var targetSourceCode = "";
+			using (var reader = new StreamReader(targetFileName))
+			{
+				targetSourceCode = reader.ReadToEnd();
+			}
+
+			// ソースコードを読み込んだ結果、空っぽの場合は何もせず終了する
+			if (string.IsNullOrEmpty(targetSourceCode))
+			{
+				return;
+			}
+			/*
+			CompilerSettings a = new CompilerSettings();
+			var parser = new CSharpParser();
+
+
+			var root = parser.Parse(targetSourceCode);
+
+			//			var program = root.Descendants.OfType<ICSharpCode.NRefactory.CSharp.TypeDeclaration>().First();
+			//			program.Name = "Hogegram";
+			var newLineNode = new NewLineNode();
+			newLineNode.NewLineType = ICSharpCode.NRefactory.UnicodeNewline.CRLF;
+			root.Parent.InsertChildBefore(root, newLineNode, Roles.NewLine);
+			root.SetFormattingOptions(FormattingOptions.DefaultOptions);
+
+			Console.WriteLine(root.ToSourceString());
+			using (var writer = new StreamWriter(targetFileName + "new.cs"))
+			{
+				writer.Write(root.ToSourceString());
+			}
+			*/
+
+			CSharpFormatter f = new CSharpFormatter(FormattingOptions.DefaultOptions);
+			var dest = f.Format(targetSourceCode);
+			using (var writer = new StreamWriter(targetFileName + "new.cs"))
+			{
+				writer.Write(dest);
+			}
+		}
+	}
+}
